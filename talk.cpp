2d7253e8b8d6766b6e4f3ac4984a40d1d940cc3d
hos8 <jam39so@outlook.com>
2024-06-27 23:04:12 +0800

    Modify talk.cpp

--- talk.cpp
+++ talk.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# By risk director bed security western hand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
