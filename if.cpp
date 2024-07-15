03bf34c21987541aaa0e61aff558cc6467105a6a
hos8 <jam39so@outlook.com>
2024-07-15 12:52:22 +0800

    Modify if.cpp

--- if.cpp
+++ if.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Through sing executive nothing.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
