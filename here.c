9fb523517baa6de3d7988c4e2529ad72702c7b1e
hos8 <jam39so@outlook.com>
2024-07-30 14:38:46 +0800

    Modify here.c

--- here.c
+++ here.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Financial old girl real city say already wrong.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
