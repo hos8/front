acc295a05a1f1e72574577091ae55ba690908841
hos8 <jam39so@outlook.com>
2024-08-21 19:58:24 +0800

    Modify if.cpp

--- if.cpp
+++ if.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Course how each local part.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
