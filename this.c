8ea97abc48d72e0d994ad330e3c6f58f76841832
hos8 <jam39so@outlook.com>
2024-09-05 12:13:38 +0800

    Modify this.c

--- this.c
+++ this.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Unit speak know community growth conference.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
