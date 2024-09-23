948007f889cc7494deb61492e54e9d2e0118f182
hos8 <jam39so@outlook.com>
2024-09-23 12:06:38 +0800

    Modify discussion.cpp

--- discussion.cpp
+++ discussion.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Wrong friend coach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
