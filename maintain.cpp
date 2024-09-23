093448da37bc05bb92c079781ad29f2266e4c6c5
hos8 <jam39so@outlook.com>
2024-09-23 12:06:38 +0800

    Modify maintain.cpp

--- maintain.cpp
+++ maintain.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Design actually parent hand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
