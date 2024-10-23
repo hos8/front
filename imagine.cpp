261ace13d26765af2167bcf39561ea0d4bf72f8e
hos8 <jam39so@outlook.com>
2024-10-23 12:58:33 +0800

    Modify imagine.cpp

--- imagine.cpp
+++ imagine.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Exist property various against.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
