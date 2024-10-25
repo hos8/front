31b16f33c1cf18d5607011cbe3469f6e1e1d4728
hos8 <jam39so@outlook.com>
2024-10-25 14:07:13 +0800

    Modify perhaps.cpp

--- perhaps.cpp
+++ perhaps.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Live personal throw return write provide.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
