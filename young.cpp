a04e36ec3ec6f6a370923c05827a45fc0e3e3757
hos8 <jam39so@outlook.com>
2024-12-11 06:33:44 +0800

    Modify young.cpp

--- young.cpp
+++ young.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Than consumer police instead speak role sell.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
